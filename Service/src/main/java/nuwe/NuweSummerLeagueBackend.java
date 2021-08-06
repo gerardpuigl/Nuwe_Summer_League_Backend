be6f32bc6ccb89468bc2ffdade2a02b8c5015eed
Gerard <gerardpuig31@hotmail.com>
2021-08-06 23:30:04 +0200

    Starter Configuration

--- Service/src/main/java/nuwe/NuweSummerLeagueBackend.java
+++ Service/src/main/java/nuwe/NuweSummerLeagueBackend.java
@@ -1,33 +1,13 @@
 package nuwe;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import nuwe.infraestructure.cli.CommandsController;
-import picocli.CommandLine;
-import picocli.CommandLine.IFactory;
-
 @SpringBootApplication
-public class NuweSummerLeagueBackend implements CommandLineRunner {
+public class NuweSummerLeagueBackend {
 
 	public static void main(String[] args) {
-		System.out.println("Processing...");
 		SpringApplication.run(NuweSummerLeagueBackend.class, args);
 	}
-
-	@Autowired
-    private IFactory factory;
-	
-	@Autowired
-	private CommandsController commandsController;
-	
-	@Override
-	public void run(String... args) {
-		CommandLine commandLine = new CommandLine(commandsController, factory);
-		commandLine.execute(args);
-	}
-	
 	
 }
